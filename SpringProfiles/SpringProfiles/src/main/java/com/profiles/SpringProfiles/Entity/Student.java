c433464134fcbc30c4649a4ab61b0f02a04f6451
AjitSurvase <ajitsurvase71@gmail.com>
2025-12-12 00:53:48 +0530

    spring profiles

---
+++ SpringProfiles/SpringProfiles/src/main/java/com/profiles/SpringProfiles/Entity/Student.java
@@ -0,0 +1,66 @@
+package com.profiles.SpringProfiles.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+
+@Entity
+public class Student {
+	
+	
+	@Id
+	private int id;
+	
+	private String Name;
+
+	private String email;
+
+	
+	
+	
+
+	
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int id, String name, String email) {
+		super();
+		this.id = id;
+		Name = name;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+	
+	
+}
